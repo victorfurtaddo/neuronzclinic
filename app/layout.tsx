3685a77b8b7bdd0397c0345fc444eefa72971e1f
Gabriel de Almeida Martins <gabriel_241@outlook.com.br>
2026-05-11 18:26:27 -0300

    hotfix: Correção do theme-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased flex h-screen overflow-hidden`}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
+        <ThemeProvider attribute="class">
           <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
 
           <main className="flex-1 overflow-y-auto bg-background">{children}</main>
